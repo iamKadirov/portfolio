16c70ab66a60742715a1e59be4a05e6528389399
iamKadirov <criro9891@gmail.com>
2025-06-13 14:48:22 +0500

    change gmail

--- components/contact.tsx
+++ components/contact.tsx
@@ -36,7 +36,7 @@ export default function Contact() {
                       <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                       <polyline points="22,6 12,13 2,6"></polyline>
                     </svg>
-                    criro9891@gmail.com
+                    kadirovabduvosit102@gmail.com
                   </Link>
                 </Button>
 
